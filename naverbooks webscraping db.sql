76607945c956636c7b75a2b0c16ae49cc207b9d4
syjo6658 <75193169+syjo6658@users.noreply.github.com>
2022-08-18 14:49:23 +0900

    Add files via upload

--- naverbooks webscraping db.sql
+++ naverbooks webscraping db.sql
@@ -25,18 +25,29 @@ create table bestsellers (
 select * from newbooks;
 select * from bestsellers;
 
+-- 장르별 신간 도서 수
+select genre, count(*) from newbooks
+	group by genre
+    order by count(*) desc;
 -- ----------------------------------------------------------------------------------------------------------------
+-- 신간 / 베스트셀러 비교 - 도서명
 SELECT A.BOOK_ID, A.TITLE, A.AUTHOR, A.GENRE, A.GENRE_RANK, A.PUBLISHER, A.PUBLICATION_DATE
 	FROM NEWBOOKS A
     INNER JOIN BESTSELLERS B
     ON A.TITLE = B.TITLE;
     
-SELECT A.BOOK_ID, A.TITLE, A.AUTHOR, A.GENRE, A.GENRE_RANK, A.PUBLISHER, A.PUBLICATION_DATE
+-- 신간 / 베스트셀러 비교 - 저자명
+SELECT A.AUTHOR, A.GENRE, count(*)
 	FROM NEWBOOKS A
     INNER JOIN BESTSELLERS B
-    ON A.AUTHOR = B.AUTHOR;
-    
-SELECT A.BOOK_ID, A.TITLE, A.AUTHOR, A.GENRE, A.GENRE_RANK, A.PUBLISHER, A.PUBLICATION_DATE
+    ON A.AUTHOR = B.AUTHOR
+    group by a.genre
+    order by count(*) desc;
+
+-- 신간 / 베스트셀러 비교 - 출판사명    
+SELECT A.PUBLISHER, A.GENRE, count(*)
 	FROM NEWBOOKS A
     INNER JOIN BESTSELLERS B
-    ON A.PUBLISHER = B.PUBLISHER;
+    ON A.PUBLISHER = B.PUBLISHER
+    group by a.genre
+    order by count(*) desc;
